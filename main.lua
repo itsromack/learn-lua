1337a47779e7b79aa584595a8de0448ec26dd759
Romack Natividad <romacknatividad@gmail.com>
2018-02-06 01:56:00 +0800

    Change BG color

--- main.lua
+++ main.lua
@@ -1,13 +1,21 @@
+--Draw the display
 function love.draw()
+	love.graphics.print("Hi, hows it going? I'm Macki", 10, 10)
 	love.graphics.draw(romack, 300, 200)
 end
 
+--Load Assets
 function love.load()
+	local myfont = love.graphics.newFont(45)
+	love.graphics.setFont(myfont)
+	--love.graphics.setColor(0,0,0,255)
+	--love.graphics.setBackgroundColor(255,153,0)
 	romack = love.graphics.newImage("images/romack.png")
 	fight_audio = love.audio.newSource("audio/fight.ogg")
 	love.audio.play(fight_audio)
 end
 
-function hello_world()
-	love.graphics.print('Hello World', 400, 300)
+--Update Event
+function love.update(dt)
+--do the maths here
 end
